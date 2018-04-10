8314ce2f9cf2672b72ceaaaa75279d867fb16d31
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-10 14:01:12 -0400

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,39 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
+# "SELECT color, COUNT(color)
+  # FROM bears
+  # WHERE grade = 9
+  # GROUP BY color
+  # HAVING count(*) > 1 
+  # ORDER BY color DESC
+  # LIMIT 1;"
+
 def highest_student_gpa
+  "SELECT gpa
+  FROM students
+  ORDER BY gpa DESC
+  LIMIT 1;"
 end
 
 def lowest_student_gpa
+  "SELECT gpa
+  FROM students
+  ORDER BY gpa
+  LIMIT 1;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa)
+  FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies)
+  FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa)
+  FROM students
+  WHERE grade = 9;"
 end
